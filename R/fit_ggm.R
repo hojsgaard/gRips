204545532b9ebb530d2868c25c4545de8f8ccb55
Steffen Lauritzen <lauritzen@math.ku.dk>
2023-11-15 12:35:35 +0100

    byttet i fit_ggm.R

--- R/fit_ggm.R
+++ R/fit_ggm.R
@@ -78,6 +78,8 @@ fit_ggm <- function(S, formula=NULL, nobs, K=NULL, maxit=10000L, eps=1e-2, convc
         S <- cov2cor(S)
     }
 
+    max_coreness <- check_coreness(emat, nrow(S), nobs)
+    
     S <- smart_start(S, amat)
     
     max_coreness <- check_coreness(emat, nrow(S), nobs)
