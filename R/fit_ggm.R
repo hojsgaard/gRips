74f2242d21bd9cba0250a745bc1fd1348e5800cd
Steffen Lauritzen <lauritzen@math.ku.dk>
2023-11-17 12:08:07 +0100

    gaaet tilbage til smartstart

--- R/fit_ggm.R
+++ R/fit_ggm.R
@@ -83,7 +83,8 @@ fit_ggm <- function(S, formula=NULL, nobs, K=NULL, maxit=10000L, eps=1e-2, convc
     maxdeg <- max(deg)
     
     if ((identical(method,"ncd"))&& (maxdeg>= nobs-1)){
-       S <- sweep_start(S, amat,eps=eps,f=nobs-1) 
+       ## S <- sweep_start(S, amat,eps=eps,f=nobs-1)
+      S <- smart_start(S, amat)
     }
 
     switch(method,
