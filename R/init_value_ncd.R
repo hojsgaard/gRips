29b2f050bb8f0eb78f099d8d024367c81a524cd7
Søren Højsgaard <sorenh@math.aau.dk>
2023-11-20 22:21:12 +0100

    SH: minor

--- R/init_value_ncd.R
+++ R/init_value_ncd.R
@@ -1,3 +1,39 @@
+quick_start <- function(S, good, amat) {
+  d <- nrow(amat)
+  Sigma <- S
+  
+  for (i in good)
+  {
+    b <- get_bd(1:d, i, amat)
+    
+    Sigma <- update_(Sigma, i, b)
+  }
+  return(Sigma)
+}
+
+full_start <- function(S, amat) {
+  d <- nrow(amat)
+  tmp <- small_first(amat)
+  S2 <- S[tmp$sfo, tmp$sfo]
+  amat2 <- amat[tmp$sfo, tmp$sfo]
+  for (i in 1:d) {
+    b <- get_bd(1:d, i, amat2)
+    S2 <- update_(S2, i, b)
+  }
+  return(S2[tmp$sfo_inv, tmp$sfo_inv])
+}
+
+
+
+small_first <- function(amat) {
+  sfo <- smallest_first_ordering(amat)
+  nms <- rownames(amat)
+  sfo_inv <- match(1:nrow(amat), sfo)
+
+  return(list(sfo=sfo, sfo_nms=nms[sfo],
+              sfo_inv=sfo_inv,
+              sfo_inv_nms=nms[sfo_inv]))
+}
 
 
 get_bd <- function(A, u, amat){
@@ -21,68 +57,6 @@ update_ <- function(A, u, b) {
 }
 
 
-
-# 
-# small_last <- function(amat, f) {    
-#   slo <- smallest_last_ordering(amat)
-#   nms <- rownames(amat)
-#   slo_inv <- match(1:nrow(amat), slo)
-#   amat2 <- amat[slo, slo]
-#   
-#   d <- nrow(amat)
-#   A <- 1:d
-#   b <- vector("list", d-1)
-#   b_nms <- vector("list", d-1)
-#   
-#   for (i in d:1) {
-#     bi <- intersect(get_bd(A, i, amat2), A)
-#     deg <- length(bi)
-#  ##   if (f <= deg){
-#  ##     stop("MLE may not exist\n") SLL: redundant naar coreness er checket
-#  ##   }
-#     b[[i]] <- bi
-#     b_nms[[i]] <- nms[slo][bi]
-#     A <- A[-i]
-#   }
-#   names(b) <- nms[slo]
-#   names(b_nms) <- nms[slo]
-#   return(list(slo=slo, slo_nms=nms[slo],
-#               slo_inv=slo_inv,
-#               slo_inv_nms=nms[slo_inv],
-#               b_nms=b_nms,
-#               b=b))
-# }
-
-# 
-# 
-# smallest_last_ordering <- function(adj_matrix) {
-#   # Calculate the degrees of each node (number of neighbors)
-#   degrees <- rowSums(adj_matrix)
-#   
-#   ## Initialize an empty vector to store the smallest-last ordering
-#   ordering <- numeric(length = ncol(adj_matrix))
-#   passive <- numeric(length = ncol(adj_matrix))
-#   
-#   for (i in 1:length(ordering)) {
-#     ## Find the node with the smallest degree
-#     degrees
-#     smallest_degree_node <- which(degrees == min(degrees))[1]
-#     smallest_degree_node
-#     
-#     ## Update the ordering and remove the node and its edges
-#     ordering[i] <- smallest_degree_node
-#     adj_matrix[smallest_degree_node,] <- 0
-#     adj_matrix[,smallest_degree_node] <- 0
-#     degrees <- rowSums(adj_matrix)
-#     degrees[smallest_degree_node] <- Inf  # Set the degree of the removed node to Inf
-#     passive[smallest_degree_node] <- Inf
-#     degrees <- degrees + passive
-#     ## str(list(passive=passive, degrees=degrees, ordering=ordering))
-#     
-#   }
-#   
-#   return(rev(ordering))
-# }
 
 smallest_first_ordering <- function(adj_matrix) {
   # Calculate the degrees of each node (number of neighbors)
@@ -115,28 +89,6 @@ smallest_first_ordering <- function(adj_matrix) {
 
 
 
-small_first <- function(amat) {
-  sfo <- smallest_first_ordering(amat)
-  nms <- rownames(amat)
-  sfo_inv <- match(1:nrow(amat), sfo)
-
-  return(list(sfo=sfo, sfo_nms=nms[sfo],
-              sfo_inv=sfo_inv,
-              sfo_inv_nms=nms[sfo_inv]))
-}
-
-
-full_start <- function(S, amat) {
-  d <- nrow(amat)
-  tmp <- small_first(amat)
-  S2 <- S[tmp$sfo, tmp$sfo]
-  amat2 <- amat[tmp$sfo, tmp$sfo]
-  for (i in 1:d) {
-    b <- get_bd(1:d, i, amat2)
-    S2 <- update_(S2, i, b)
-  }
-  return(S2[tmp$sfo_inv, tmp$sfo_inv])
-}
 
 
 
@@ -206,18 +158,68 @@ full_start <- function(S, amat) {
 # }
 
 
-quick_start <- function(S, good, amat) {
-  d <- nrow(amat)
-  Sigma <- S
-  
-  for (i in good)
-  {
-    b <- get_bd(1:d, i, amat)
-    
-    Sigma <- update_(Sigma, i, b)
-  }
-  return(Sigma)
-}
 
 
 
+
+# 
+# small_last <- function(amat, f) {    
+#   slo <- smallest_last_ordering(amat)
+#   nms <- rownames(amat)
+#   slo_inv <- match(1:nrow(amat), slo)
+#   amat2 <- amat[slo, slo]
+#   
+#   d <- nrow(amat)
+#   A <- 1:d
+#   b <- vector("list", d-1)
+#   b_nms <- vector("list", d-1)
+#   
+#   for (i in d:1) {
+#     bi <- intersect(get_bd(A, i, amat2), A)
+#     deg <- length(bi)
+#  ##   if (f <= deg){
+#  ##     stop("MLE may not exist\n") SLL: redundant naar coreness er checket
+#  ##   }
+#     b[[i]] <- bi
+#     b_nms[[i]] <- nms[slo][bi]
+#     A <- A[-i]
+#   }
+#   names(b) <- nms[slo]
+#   names(b_nms) <- nms[slo]
+#   return(list(slo=slo, slo_nms=nms[slo],
+#               slo_inv=slo_inv,
+#               slo_inv_nms=nms[slo_inv],
+#               b_nms=b_nms,
+#               b=b))
+# }
+
+# 
+# 
+# smallest_last_ordering <- function(adj_matrix) {
+#   # Calculate the degrees of each node (number of neighbors)
+#   degrees <- rowSums(adj_matrix)
+#   
+#   ## Initialize an empty vector to store the smallest-last ordering
+#   ordering <- numeric(length = ncol(adj_matrix))
+#   passive <- numeric(length = ncol(adj_matrix))
+#   
+#   for (i in 1:length(ordering)) {
+#     ## Find the node with the smallest degree
+#     degrees
+#     smallest_degree_node <- which(degrees == min(degrees))[1]
+#     smallest_degree_node
+#     
+#     ## Update the ordering and remove the node and its edges
+#     ordering[i] <- smallest_degree_node
+#     adj_matrix[smallest_degree_node,] <- 0
+#     adj_matrix[,smallest_degree_node] <- 0
+#     degrees <- rowSums(adj_matrix)
+#     degrees[smallest_degree_node] <- Inf  # Set the degree of the removed node to Inf
+#     passive[smallest_degree_node] <- Inf
+#     degrees <- degrees + passive
+#     ## str(list(passive=passive, degrees=degrees, ordering=ordering))
+#     
+#   }
+#   
+#   return(rev(ordering))
+# }
